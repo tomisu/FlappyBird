d22e7e6683f3df4e470983a654e1b22222f40945
cubar10 <alejandrotatum@gmail.com>
2025-11-25 16:28:25 +0100

    Añadir AlexFullController y lógica mejorada para el pájaro alex

--- src/Controllers/Tomas.js
+++ src/Controllers/Tomas.js
@@ -1,30 +1,47 @@
 
 
-export default class TomasController {
+export default class AlexFullController {
   constructor(jump) {
     /*
     ** Jump is a function that accepts a jump strength (1-3) and makes the bird jump.
     ** The higher the strength, the bigger the jump
     */
     this.jump = jump;
-    this.name = 'Tomás';
+    this.name = 'alex';
   }
 
   loop(birdData, pipes, scene, dt) {
-    /*
-    ** This function is called every frame.
-    **
-    ** birdData = {x: int, y: int, speed: int }
-    ** pipes = array of Pipe
-    ** Pipe = {x: int, isTop: boolean, pipeHeight: int, pipeWidth: int}
-    ** scene: {width: 800, height: 600, floor: int (the floor height)}
-    ** dt: milliseconds elapsed since last update
-    */
-
     if (birdData.y > scene.height - scene.floor - 10) {
-      this.jump(3);
+      this.jump(2);
+      return;
+    }
+
+    if (birdData.y < 60) {
+      return;
+    }
+
+    let nextPipe = null;
+    for (const pipe of pipes) {
+      if (pipe.x + pipe.pipeWidth > birdData.x) {
+        if (!nextPipe || pipe.x < nextPipe.x) {
+          nextPipe = pipe;
+        }
+      }
+    }
+
+    if (nextPipe) {
+      let gapTop = nextPipe.isTop ? nextPipe.pipeHeight : 0;
+      let gapBottom = nextPipe.isTop ? scene.height - scene.floor : nextPipe.pipeHeight + nextPipe.pipeWidth;
+      let gapCenter = gapTop + (gapBottom - gapTop) / 2;
+
+      if (birdData.y > gapCenter + 15 && birdData.speed > 0) {
+        this.jump(1);
+      }
+      else if (birdData.y > gapCenter + 40 && birdData.speed > 2) {
+        this.jump(2);
+      }
     }
   }
 }
 
-export const color = 0xff00ff;
+export const color = 0x0;
